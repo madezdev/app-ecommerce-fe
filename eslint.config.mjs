4173b47ba6928fe4fdec6aeb97d990796a0f76f0
Martin Hernandez <madezdev@gmail.com>
2025-02-10 21:33:30 -0300

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,44 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ["next/core-web-vitals"],
+    plugins: ["@typescript-eslint"],
+    env: {
+      node: true,
+    },
+    rules: {
+      semi: ["error", "never"],
+      "indent": ["error", 2],
+      "no-unused-vars": ["warn"],
+      "no-unused-vars": ["error"],
+      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+      "no-trailing-spaces": "error",
+      "space-infix-ops": "error",
+      "comma-spacing": ["error", { "before": false, "after": true }],
+      "space-before-blocks": ["error", "always"],
+      "space-before-function-paren": ["error", "always"],
+      "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 0, "maxBOF": 0 }],
+      "curly": ["error", "all"], // Requiere llaves en todas las estructuras de control (if, for, while)
+      "eqeqeq": ["error", "always"], // Prohíbe el uso de `==` en lugar de `===`
+      "quotes": ["error", "double", { "avoidEscape": true }],
+      "object-curly-spacing": ["error", "always"], // Requiere espacios dentro de `{ objetos }`
+      "array-bracket-spacing": ["error", "never"], // No permite espacios dentro de `[ arreglos ]`
+      "space-before-function-paren": ["error", "always"], // Espacio antes de `function ()`
+      "key-spacing": ["error", { "beforeColon": false, "afterColon": true }],
+      "@typescript-eslint/no-explicit-any": "error", // Prohíbe el uso de `any`
+      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+    },
+  }),
+]
 
-export default eslintConfig;
+export default eslintConfig
