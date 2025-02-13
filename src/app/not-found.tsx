76d4140ba6512cb9a94dd0ae6fafbb30137b10eb
Martin Hernandez <madezdev@gmail.com>
2025-02-12 21:28:34 -0300

    Agregar componente NotFound para manejar errores 404; incluir diseño y enlaces de navegación

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import { NotFound } from '@/components/ui/not-found/notFound'
+
+export default function () {
+  return (
+    <div className='h-screen w-screen'>
+      <NotFound />
+    </div>
+  )
+}
