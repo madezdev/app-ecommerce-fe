ed40b84b80e886c22dfcf292e9f4a375c4327716
Martin Hernandez <madezdev@gmail.com>
2025-02-12 21:33:52 -0300

    Agregar fondo gris al componente NotFound para mejorar la visibilidad

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import { NotFound } from '@/components/ui/not-found/notFound'
 
 export default function () {
   return (
-    <div className='h-screen w-screen'>
+    <div className='h-screen w-screen bg-gray-100'>
       <NotFound />
     </div>
   )
