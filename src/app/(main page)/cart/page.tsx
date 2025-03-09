dca4bd325d7b16c530c0079af6037ae5402462e2
Martin Hernandez <madezdev@gmail.com>
2025-03-09 19:58:05 -0300

    Agregar componente de información de envío al carrito de compras y mejorar la estructura del resumen de orden

--- src/app/(main page)/cart/page.tsx
+++ src/app/(main page)/cart/page.tsx
@@ -3,6 +3,7 @@ import { Title } from '@/components/ui/font-style/title'
 import { ProductsInCart } from '@/components/cart/productsInCart'
 import { Header } from '@/components/ui/header/header'
 import { PurchaseSummary } from '@/components/cart/purchaseSummary'
+import { ShippingInformation } from '@/components/cart/shippingInformation'
 
 export default function () {
   // redirect('/empty');
@@ -10,18 +11,21 @@ export default function () {
   return (
     <>
       <Header title='Carrito de compras' img='hero-cart-sostenible' />
-      <div className="xl:container mx-4 lg:mx-8 xl:mx-auto mb-[20px] lg:mb-72 mt-[20px] sm:px-0">
+      <div className="xl:container mx-4 lg:mx-8 xl:mx-auto my-[20px] pb-[40px] sm:px-0">
 
         <Title title='Tus productos' />
         <div className="flex flex-col items-center justify-between xl:flex-row xl:items-start gap-10 w-full mt-4">
           {/* Carrito */ }
-          <div className="flex flex-col gap-4 w-full xl:w-3/4">
+          <div className="flex flex-col gap-4 w-full xl:w-3/4 ">
             {/* Items */ }
             <ProductsInCart />
           </div>
 
           {/* Checkout - Resumen de orden */ }
-          <PurchaseSummary />
+          <div className="flex flex-col gap-4 flex-shrink-0">
+            <PurchaseSummary />
+            <ShippingInformation />
+          </div>
         </div>
 
       </div>
